0212cd6cca0820e6e4d2f1f0b8a66fc0fb65bc2e
Ghislain Bourgeois <ghislain.bourgeois@gmail.com>
2017-06-13 18:23:24 -0400

    Add meetup name

--- src/meetup_loto.py
+++ src/meetup_loto.py
@@ -2,8 +2,9 @@ import random
 
 
 class Loto:
-    def __init__(self, api, event_id):
+    def __init__(self, api, meetup_name, event_id):
         self.api = api
+        self.meetup_name = meetup_name
         self.event_id = event_id
         self.participants = self._get_participants_member_ids()
 
@@ -22,5 +23,5 @@ class Loto:
             return 0
 
     def _get_participants_member_ids(self):
-        rsvps = self.api.get_rsvps(self.event_id)
+        rsvps = self.api.get_rsvps(self.meetup_name, self.event_id)
         return [rsvp['member']['id'] for rsvp in rsvps]
--- tests/test_meetup_loto.py
+++ tests/test_meetup_loto.py
@@ -6,7 +6,7 @@ from meetup_loto import Loto
 class TestMeetupLoto(unittest.TestCase):
     def setUp(self):
         self.api = FakeApi()
-        self.loto = Loto(self.api, 240672864)
+        self.loto = Loto(self.api, "Docker-Montreal", 240672864)
 
     def test_meetup_loto_participants(self):
         nb_participants = self.loto.number_of_participants()
@@ -27,5 +27,5 @@ class TestMeetupLoto(unittest.TestCase):
 
 
 class FakeApi:
-    def get_rsvps(self, event_id):
+    def get_rsvps(self, meetup_name, event_id):
         return [{'member': {'id': 1}}, {'member': {'id': 2}}, {'member': {'id': 3}}, {'member': {'id': 4}}, {'member': {'id': 5}}, {'member': {'id': 6}}, {'member': {'id': 7}}, {'member': {'id': 8}}, {'member': {'id': 9}}, {'member': {'id': 10}}]
